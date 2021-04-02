23d20c45bdc926440516e69c169182c4d8513483
Anentropic <ego@anentropic.com>
2021-04-02 23:41:52 +0100

    WIP PyO3 interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
+use std::fmt::Debug;
+
 use pyo3::prelude::*;
 
-use glsl_shaderinfo::{get_info, ShaderInfo};
+use glsl_shaderinfo::{get_info, ShaderInfo, VarInfo, BlockInfo, FieldInfo};
 
 mod glsl_shaderinfo;
+
+
+#[pyclass]
+#[derive(Clone, Debug, Default, Send)]
+pub struct WTFInfo {
+    pub name: String,
+    pub storage: Option<String>,
+    pub type_name: String,
+    pub array: Option<Vec<usize>>,
+}
 
 
 #[pymodule]
@@ -14,5 +26,12 @@ fn glsl_shaderinfo(_py: Python, m: &PyModule) -> PyResult<()> {
         Ok(result)
     }
 
+    m.add_class::<WTFInfo>()?;
+
+    // m.add_class::<ShaderInfo>()?;
+    // m.add_class::<VarInfo>()?;
+    // m.add_class::<BlockInfo>()?;
+    // m.add_class::<FieldInfo>()?;
+ 
     Ok(())
 }
